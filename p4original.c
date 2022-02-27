13b19363af4366e8b5f13f19d125ecdfa7efc218
Ankit Patil <96898434+Ankitpatil9901@users.noreply.github.com>
2022-02-27 15:39:58 +0000

    nothing

---
+++ p4original.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int input()
+{
+  int x;
+  printf("Enter the value of x");
+  scanf("%d",&x);
+  return x;
+}
+int find_fibo(int n)
+{
+  int a=0,b=1,c=0;
+  for(int i=2;i<=n;i++)
+    {
+      c=a+b;
+      a=b;
+      b=c;
+    }
+  return (c-1);
+}
+void output(int n,int fibo)
+{
+  printf("%d th fibonacci number is %d",n,fibo);
+}
+int main()
+{
+  int x,fibo;
+  x=input();
+  fibo=find_fibo(x);
+  output(x,fibo);
+  return 0;
+}
---
+++ p5original.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int input()
+{
+  int n;
+  printf("Enter the value of n\n");
+  scanf("%d",&n);
+  return n;
+  }
+void input_array(int n,inta[n])
+{
+  printf("Enter the array elements\n");
+  for(int i=0;i<n;i++)
+    {
+    a[i]=i;
+  a[1]=0;
+}
+  }
+void erotosthenes_sieve(int n,int a[n])
+{
+  int i=0;
+  while(i < sqrt(n))
+    {
+      while(a[i]==0)i++;
+      for(int k=i+i;i,n;k+=i)
+      a[k]=0;
+      i++;
+      }
+}
+
+void out_put(int n,int a[n])
+{
+  int i;
+  for(i=2;i<)
+  int x
+}
