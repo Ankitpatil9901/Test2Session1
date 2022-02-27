4dccc46c1e530320eb93e75a04fcb5e5b36fcf94
Ankit Patil <96898434+Ankitpatil9901@users.noreply.github.com>
2022-02-27 15:50:10 +0000

    no

--- p5original.c
+++ p5original.c
@@ -1,35 +1,57 @@
 #include<stdio.h>
-int input()
+#include <math.h>
+int input_array_size()
 {
   int n;
-  printf("Enter the value of n\n");
+  printf("enter the size of the array\n");
   scanf("%d",&n);
   return n;
-  }
-void input_array(int n,inta[n])
+}
+void input_array(int n,int a[n])
 {
-  printf("Enter the array elements\n");
   for(int i=0;i<n;i++)
     {
-    a[i]=i;
+      a[i]=i;
+    }
   a[1]=0;
 }
-  }
 void erotosthenes_sieve(int n,int a[n])
 {
   int i=0;
-  while(i < sqrt(n))
-    {
-      while(a[i]==0)i++;
-      for(int k=i+i;i,n;k+=i)
-      a[k]=0;
-      i++;
+  while (i<sqrt(n))
+  {
+      while(a[i]!=0)
+      {
+        int f;
+        for(f=i+i;f<n;f+=i)
+        {
+         a[f]=0;
+        }
+        break;
       }
+    i++;  
+  }
+  
 }
-
 void out_put(int n,int a[n])
 {
   int i;
-  for(i=2;i<)
-  int x
+  for(i=2;i<n;i++)
+    {
+      while(a[i]!=0)
+        {
+          printf("%d,",a[i]);
+          break;
+        }
+    }
+}
+int main()
+{
+  int x;
+  x=input_array_size();
+  int y[x];
+  input_array(x,y);
+  erotosthenes_sieve(x,y);
+  out_put(x,y);
+  return 0;
 }
